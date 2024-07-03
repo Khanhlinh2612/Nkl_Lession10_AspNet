d69030a3229691b867af95dc0f2dc63b22c85cd8
Khanhlinh2612 <linhkhanh261258@gmail.com>
2024-07-03 11:22:14 +0700

    Lession10

---
+++ NklK22CNT3Lession10/NklK22CNT3Lession10/Controllers/NklHomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThpK22CNT3Lession10.Models;
+
+namespace ThpK22CNT3Lession10.Controllers
+{
+    public class ThpHomeController : Controller
+    {
+        public ActionResult ThpIndex()
+        {
+            //Check data from sesstion
+            if (Session["ThpAccount"] !=null)
+            {
+                var thpAccount = Session["ThpAccount"] as ThpAccount;
+                ViewBag.FullName= thpAccount.ThpFullName;
+            }
+            return View();
+        }
+
+        public ActionResult ThpAbout()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult ThpContact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ NklK22CNT3Lession10/NklK22CNT3Lession10/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThpK22CNT3Lession10.MvcApplication" Language="C#" %>
